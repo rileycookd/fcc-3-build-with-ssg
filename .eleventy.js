93ae7df3e6210558ad4a7d0f0c9037caafb7790d
Riley Cook <rileycookd@gmail.com>
2020-04-01 18:40:27 -0300

    add weather api

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,9 @@
 module.exports = function(config) {
   
+  config.setBrowserSyncConfig({
+    https: true
+  });
+
   config.addPassthroughCopy("src/js");
   return {
     dir: {
@@ -10,3 +14,60 @@ module.exports = function(config) {
   };
 
 };
+
+// [
+//   "ae",
+//   "ar",
+//   "at",
+//   "au",
+//   "be",
+//   "bg",
+//   "br",
+//   "ca",
+//   "ch",
+//   "cn",
+//   "co",
+//   "cu",
+//   "cz",
+//   "de",
+//   "eg",
+//   "fr",
+//   "gb",
+//   "gr",
+//   "hk",
+//   "hu",
+//   "id",
+//   "ie",
+//   "il",
+//   "in",
+//   "it",
+//   "jp",
+//   "kr",
+//   "lt",
+//   "lv",
+//   "ma",
+//   "mx",
+//   "my",
+//   "ng",
+//   "nl",
+//   "no",
+//   "nz",
+//   "ph",
+//   "pl",
+//   "pt",
+//   "ro",
+//   "rs",
+//   "ru",
+//   "sa",
+//   "se",
+//   "sg",
+//   "si",
+//   "sk",
+//   "th",
+//   "tr",
+//   "tw",
+//   "ua",
+//   "us",
+//   "ve",
+//   "za"
+// ]
